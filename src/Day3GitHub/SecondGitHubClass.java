b0f0268b65f3b4c2765baf30b5dd86403356004e
SevgiAras <123619012+SevgiAras@users.noreply.github.com>
2023-01-31 21:36:09 -0700

    Update SecondGitHubClass.java
    hello  changed to hey

--- src/Day3GitHub/SecondGitHubClass.java
+++ src/Day3GitHub/SecondGitHubClass.java
@@ -2,7 +2,7 @@ package Day3GitHub;
 
 public class SecondGitHubClass {
     public static void main(String[] args) {
-        System.out.println("Hello Calgary");
+        System.out.println("Hey Calgary");
         System.out.println(1+1);
 
     }
