29ee32632e26b02b2755be71f0140d7993780d1e
user <sevgi012127@gmail>
2023-01-31 21:12:29 -0700

    second file

---
+++ src/Day3GitHub/SecondGitHubClass.java
@@ -0,0 +1,9 @@
+package Day3GitHub;
+
+public class SecondGitHubClass {
+    public static void main(String[] args) {
+        System.out.println("Hello Calgary");
+        System.out.println(1+1);
+
+    }
+}
